bc57e87fbf186930edcf4d417c2b76019cd95aa6
Simone Connemara <simoneconnemara@outlook.com>
2019-10-24 16:33:52 -0500

    started superhero project

---
+++ SuperHero/Controllers/SuperheroController.cs
@@ -0,0 +1,99 @@
+﻿using SuperHero.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHero.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        ApplicationDbContext context;
+        public SuperheroController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        // GET: Superhero
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Superhero/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Superhero/Create
+        public ActionResult Create()
+        {
+            Superhero superhero = new Superhero();
+            return View(superhero);
+        }
+
+        // POST: Superhero/Create
+        [HttpPost]
+        public ActionResult Create(Superhero superhero)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                context.Superheroes.Add(superhero);
+                context.SaveChanges(); 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superhero/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Superhero superhero = context.Superheroes.Where(s => s.Id == id).FirstOrDefault();
+            return View(superhero);
+        }
+
+        // POST: Superhero/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Superhero superhero)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superhero/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Superhero/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
